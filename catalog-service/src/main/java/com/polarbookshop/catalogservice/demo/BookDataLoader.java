ff4c24d80f05fa395c5c2572556d92edb11184bb
tamlt2704 <tamlt2704@gmail.com>
2025-06-29 06:46:14 +0800

    crud repository

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
 public class BookDataLoader {
@@ -18,9 +20,9 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        bookRepository.deleteAll();
         var book1 = Book.of( "123467891", "Northern Lights", "Lyra Silverstar", 9.9);
         var book2 = Book.of("123467892", "Polar Journey", "Iorek Polarson", 12.9);
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
